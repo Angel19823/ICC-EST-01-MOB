ef22e7731d084918f46820bc1e11115d57846acb
Angel19823 <acardenasl2@est.ups.edu.ec>
2024-09-25 15:11:54 -0500

    Burbuja

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+        MetodoBurbuja metodosOrdenamiento = new MetodoBurbuja();
+
+        int[] arreglo = {5, 7, 30, 12, 9};
+        int[] arregloOrdenado = metodosOrdenamiento.sortByBubble(arreglo);
+        metodosOrdenamiento.printArreglo(arregloOrdenado);
+        System.out.print("\n -- -- -- \n");
+        int[] arregloOrdenadoDesendente = metodosOrdenamiento.sortByBubbleDesendente(arreglo);
+        metodosOrdenamiento.printArreglo(arregloOrdenadoDesendente);
+
+        //Ordenar las palabras alfabeticamente de la cadena de texto:
+        String cadena= "este es un ejemplo de texto para Ordenar";
+        String [] palabras= cadena.split(" ");
+        String [] palabrasOrdenadas= metodosOrdenamiento.sortByBubble(palabras);
+
+        metodosOrdenamiento.printArreglo(palabrasOrdenadas);
     }
 }
